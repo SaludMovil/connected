a40aa5b2f73a73cd5d3e0a6f59a0280c898b4fed
DC* <dc@syncr.com.ar>
2013-12-29 12:16:27 -0300

    Fix missing argument

--- src/Desyncr/Connected/Service/AbstractService.php
+++ src/Desyncr/Connected/Service/AbstractService.php
@@ -18,7 +18,7 @@ abstract class AbstractService implements ServiceInterface {
     }
 
 
-    public function add($key, $frame, $target) {
+    public function add($key, $frame, $target = null) {
 
         if (!is_object($frame)) {
             $frame = new BaseFrame($frame);
